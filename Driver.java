cd0f0bbd7835127901283f13487c1b5435ebccbf
Derek Isensee <derek.isensee@gmail.com>
2017-01-31 09:06:33 -0600

    Cool update!

--- Driver.java
+++ Driver.java
@@ -1,5 +1,5 @@
 package Textris;
-
+import java.util.Scanner;
 public class Driver {
     static class StartGame extends Thread {
 	String[][] board;
@@ -13,57 +13,90 @@ public class Driver {
 	
 	public void run() {
 	    try {
-		for (int i = 0; i < board.length; i++) { // print empty board
-		    for (int j = 0; j < board[i].length; j++) {
-			//Thread.sleep(70);
-			System.out.print(board[i][j]);
-		    }
-		    System.out.println();
-		}
-		while (board[21][2].equals(".")) { // this will obvoiusly cause problems. or will it
-			Tetromino t = new Tetromino(2);
-			while (t.getY() < 21) {
-			    int y = t.getX();
-			    int x = t.getY();
-			    String[] chopped = t.getShape().split("A");
-			    for (int i = 0; i < chopped.length; i++) {
-				String[] currLine = chopped[i].split("");
-				for (int j = 0; j < currLine.length; j++) {
-				    board[x][y] = "."; // we aren't printing correctly here
-				    board[x + j][y] ="*";
-				}
-				
-			    }
-			    Thread.sleep(1500);
-			    System.out.println("-------------");
-			    for (int i = 0; i < board.length; i++) {
-				for (int j = 0; j < board[0].length; j++) {
-				    System.out.print(board[i][j]);
-				}
-				System.out.println();
-			    }
-			    t.incY();
-			}
-	        }
-		    System.out.println("Game over");
+	        Scanner in = new Scanner(System.in);
+            for (int i = 0; i < board.length; i++) { // print empty board
+                for (int j = 0; j < board[i].length; j++) {
+                //Thread.sleep(70); cute animation
+                System.out.print(board[i][j]);
+                }
+                System.out.println();
+            }
+            while (board[21][2].equals(".")) { // this will obvoiusly cause problems. or will it?
+                Tetromino t = new Tetromino((int)(Math.random() * (5 - 1))); // this just works okay
+                while (t.getY() < 21) { // while the tetro hasn't reached the "current" bottom of the screen. will have to correct this.
+                    int y = t.getX(); int currY = y;
+                    int x = t.getY(); int currX = x;
+
+                    String[] chopped = t.getShape().split("A");
+
+                    if (t.getBottom() == 20 || board[x][t.getBottom()].equals("#")) { // if we reach the bottom of the board
+                        for (int i = 0; i < board.length; i++) {
+                            for (int j = 0; j < board[0].length; j++) {
+                                if (board[i][j].equals("*")) {
+                                    board[i][j] = "#";
+                                }
+                            }
+                            System.out.println();
+                        }
+
+                        System.out.println("-------------");
+                        for (int i = 0; i < board.length; i++) {
+                            for (int j = 0; j < board[0].length; j++) {
+                                System.out.print(board[i][j]);
+                            }
+                            System.out.println();
+                        }
+                        t = new Tetromino((int)(Math.random() * (5 - 1)));
+                    }
+                    else {
+                        for (int i = 0; i < board.length; i++) {
+                            for (int j = 0; j < board[0].length; j++) {
+                                if (!(board[i][j].equals("#"))) {
+                                    board[i][j] = ".";
+                                }
+                            }
+                        }
+
+                        for (int i = 0; i < chopped.length; i++) {
+                            String[] splitted = chopped[i].split("");
+                            for (int j = 0; j < splitted.length; j++) {
+                                if (!(splitted[j].equals(" "))) {
+                                    board[currX][currY + j] = splitted[j];
+                                }
+                                x = currX - 1;
+                            }
+                            currX++;
+                        }
+
+                        Thread.sleep(300);
+                        System.out.println("-------------");
+                        for (int i = 0; i < board.length; i++) {
+                            for (int j = 0; j < board[0].length; j++) {
+                                System.out.print(board[i][j]);
+                            }
+                            System.out.println();
+                        }
+                        t.incY();
+                    }
+                }
+            }
+            System.out.println("Game over");
 	    }
 	    catch (InterruptedException e) {
-		System.out.println(e.getMessage());
+		    System.out.println(e.getMessage());
 	    }
 	}
     }
     
-    public static void main (String[] args) {
-	String[][] board = new String[22][10];
-	for (int i = 0; i < board.length; i++) {
-	    for (int j = 0; j < board[i].length; j++) {
-		board[i][j] = ".";
-	    }
-	}
+	public static void main (String[] args) {
+		String[][] board = new String[22][10];
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				board[i][j] = ".";
+			}
+		}
 
-
-
-	StartGame s = new StartGame(board);
-	s.start();
+		StartGame s = new StartGame(board);
+		s.start();
     }
 }
--- Tetromino.java
+++ Tetromino.java
@@ -4,82 +4,90 @@ public class Tetromino {
     String shape;
     int x = 0;
     int y = 0;
+
     public Tetromino(int n) {
-	if (n == 1) {
-	    shape = "****";
-	    x = 3;
-	    y = 2; // should i hardcode this?
-	} else if (n == 2) {
-	    shape = "*A**A *";
-	    x = 4;
-	    y = 2;
-	} else if (n == 3) {
-	    shape = "**A**";
-	    x = 4;
-	    y = 2;
-	} else if (n == 4) {
-	    shape = "*A**A*";
-	    x = 4;
-	    y = 2;
-	} else if (n == 5) {
-	    shape = "*A*A**";
-	    x = 4;
-	    y = 2;
-	}
+        if (n == 1) {
+            shape = "****";
+            x = 3;
+            y = 2; // should i hardcode this?
+        } else if (n == 2) {
+            shape = "*A**A *";
+            x = 4;
+            y = 2;
+        } else if (n == 3) {
+            shape = "**A**";
+            x = 4;
+            y = 2;
+        } else if (n == 4) {
+            shape = "*A**A*";
+            x = 4;
+            y = 2;
+        } else if (n == 5) {
+            shape = "*A*A**";
+            x = 4;
+            y = 2;
+        }
     }
 
     public String rotate(String cur, String in) {
-	if (cur.equals("****")) { // straight tetro
-	    return "*A*A*A*";
-	}
-        if (cur.equals("*A*A*A*")) {
-	    return "****";
-	}
+        if (cur.equals("****")) { // straight tetro
+            return "*A*A*A*";
+        }
+            if (cur.equals("*A*A*A*")) {
+            return "****";
+        }
 
-	if (cur.equals("**A**")) { // square tetro
-	    return "**A**";
-	}
+        if (cur.equals("**A**")) { // square tetro
+            return "**A**";
+        }
 
-	if (cur.equals("*A**A *") && in.equals("q")) { // stuck-out tetro
-	    return " *A***";
-	}
-	if (cur.equals(" *A***") && in.equals("q")) {
-	    return " *A**A *";
-	}
-	if (cur.equals(" *A**A *") && in.equals("q")) { 
-	    return "***A *";
-	}
-	if (cur.equals("***A *") && in.equals("q")) { 
-	    return "*A**A*";
-	}
-	return "****";
+        if (cur.equals("*A**A *") && in.equals("q")) { // stuck-out tetro
+            return " *A***";
+        }
+        if (cur.equals(" *A***") && in.equals("q")) {
+            return " *A**A *";
+        }
+        if (cur.equals(" *A**A *") && in.equals("q")) {
+            return "***A *";
+        }
+        if (cur.equals("***A *") && in.equals("q")) {
+            return "*A**A*";
+        }
+        return "****";
+    }
+
+    public int getBottom() {
+        if (getShape().equals("*A**A *")) {
+            return y + 2;
+        }
+        return y + 2;
     }
 	    
     public String getShape() {
-	return shape;
+	    return shape;
     }
 
     public int getX() {
-	return x;
+	    return x;
     }
 
     public int getY() {
-	return y;
+	    return y;
     }
 
     public void incX() {
-	x++;
+	    x++;
     }
 
     public void incY() {
-	y++;
+	    y++;
     }
 
     public void decX() {
-	x--;
+	    x--;
     }
 
     public void decY() {
-	y--;
+	    y--;
     }
 }
